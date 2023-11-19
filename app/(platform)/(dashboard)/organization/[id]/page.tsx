894430e89828fd34c7143a7a27c411437578fce8
theBigBengi <imthebigbengi@gmail.com>
2023-11-20 00:58:57 +0200

    change env DATABASE_URL

--- app/(platform)/(dashboard)/organization/[id]/page.tsx
+++ app/(platform)/(dashboard)/organization/[id]/page.tsx
@@ -2,15 +2,15 @@ import { db } from "@/lib/db";
 import { Form } from "./form";
 
 export default async function Page() {
-  const boards = await db.board.findMany();
+  //   const boards = await db.board.findMany();
 
   return (
     <div className='flex flex-col space-y-4'>
       <Form />
       <div className='space-y-2'>
-        {boards.map((board: any) => (
+        {/* {boards.map((board: any) => (
           <div key={board.id}>{board.title}</div>
-        ))}
+        ))} */}
       </div>
     </div>
   );
