99f45efdba9aac69e95e6d7fce4be28157a3eecf
theBigBengi <imthebigbengi@gmail.com>
2023-11-22 01:46:27 +0200

    testc

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,6 +1,17 @@
+import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  console.log("first");
+  await db.board.create({
+    data: {
+      title: "cron test",
+      orgId: "sadfasdf",
+      imageId: "sdfsdfsdf",
+      imageThumbUrl: "sdfsdfsdf",
+      imageFullUrl: "sdfsdfsdf",
+      imageLinkHTML: "sdfsdfsdfsdf",
+      imageUserUrl: "sdfsdfsdfsdf",
+    },
+  });
   return NextResponse.json({ ok: true });
 }
